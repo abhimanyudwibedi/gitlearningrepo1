7c5da90c1a89548f3f3e920b5c0d660c11700eb4
vision x <abhikrrrrrr99992@gmail.com>
2020-12-13 20:58:47 +0530

    git innited

---
+++ Pair.java
@@ -0,0 +1,17 @@
+package arraylist;
+
+public class Pair<X,Y> {
+	X x;
+	Y y;
+	public Pair(X x,Y y) {
+		this.x=x;
+		this.y=y;
+		
+	}
+	public void getDiscreption(){
+		System.out.println(x+ " "+ y);
+		
+	}
+
+	
+}
